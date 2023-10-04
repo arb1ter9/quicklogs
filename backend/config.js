c30cf93c25ba58d84d88d1418b99a8470dfa3e7a
arb1ter <eric.gessa@gmail.com>
2023-10-04 07:44:19 -0400

    fixed dotenv

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
-require('dotenv').config()
+import dotenv from 'dotenv'
+dotenv.config();
 
 export const PORT = 5555;
 
-export const mongoDBURL = process.env.MONGODB_URL
+export const mongoDBURL = process.env.REACT_APP_MONGODB_URL
