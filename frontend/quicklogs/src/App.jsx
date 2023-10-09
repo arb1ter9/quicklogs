29a5557c8b3dcdb836e80cd7f7f98ab72ba91984
arb1ter <eric.gessa@gmail.com>
2023-10-09 16:31:02 -0400

    moving elements around

--- frontend/quicklogs/src/App.jsx
+++ frontend/quicklogs/src/App.jsx
@@ -1,101 +1,107 @@
-import { useEffect, useState } from 'react'
-import './App.css'
-import './button.css'
-import axios from 'axios'
+import { useEffect, useState } from 'react';
+import './App.css';
+import './button.css';
+import axios from 'axios';
+import { set } from 'mongoose';
 
 function App() {
-  const [tfLogs, setTfLogs] = useState([])
-  const [createdLogs, setCreatedLogs] = useState([])
-  const [logID, setLogID] = useState('')
-  const [success, setSuccess] = useState(false)
+  const [tfLogs, setTfLogs] = useState({});
+  const [createdLogs, setCreatedLogs] = useState([]);
+  const [logID, setLogID] = useState('');
+  const [success, setSuccess] = useState(true);
+  const [loading, setLoading] = useState(false);
 
-  const logsListURL = 'https://logs.tf/api/v1/log?limit=100'
-  const getLog = `http://logs.tf/api/v1/log/${logID}`
-  const dbLogList = 'http://localhost:5555/logs'
+  // const logsListURL = 'https://logs.tf/api/v1/log?limit=100';
+  const getLog = `http://logs.tf/api/v1/log/${logID}`;
+  const dbLogList = 'http://localhost:5555/logs';
 
   const handleSaveLog = async () => {
-    let logTitle = ''
-    let logMatchLength = 0
-    let logPlayers = 0
+    let logTitle = '';
+    let logMatchLength = 0;
+    let logPlayers = 0;
 
     axios
       .get(getLog)
       .then((res) => {
-        logTitle = res.data.info.title
-        logMatchLength = res.data.info.title
-        logPlayers = Object.keys(res.data.names).length
-      })
+        logTitle = res.data.info.title;
+        logMatchLength = res.data.info.title;
+        logPlayers = Object.keys(res.data.names).length;
+      });
     const data = {
       logID,
       logTitle,
       logMatchLength,
       logPlayers
-    }
+    };
 
     axios
       .post(`${dbLogList}/${logID}`, data)
       .then(() => {
-        alert('Log created!')
+        alert('Log created!');
+      })
+      .catch(() => {
+        alert('Could not add log');
+      })
+  };
+
+  const getLogData = async () => {
+    setLoading(true);
+    await axios
+      .get(getLog)
+      .then((response) => {
+        setSuccess(true);
+        setTfLogs(response.data.info);
       })
       .catch((error) => {
-        alert('Could not add log')
-        console.log(error)
+        setSuccess(false);
       })
-  }
+    setLoading(false);
+  };
 
   useEffect(() => {
     axios
       .get(dbLogList)
       .then((response) => {
-        setCreatedLogs(response.data.data)
-        createdLogs.sort()
-        console.log(createdLogs)
+        const sortedLogs = response.data.data.sort((a, b) => a.log_id - b.log_id);
+        setCreatedLogs(sortedLogs);
       })
-  }, [dbLogList])
+  }, [dbLogList]);
 
   useEffect(() => {
     if (logID !== '') {
-      axios
-        .get(getLog)
-        .then((response) => {
-          setTfLogs(response.data.info)
-          console.log(tfLogs)
-          setSuccess(true)
-        })
-        .catch((error) => {
-          setSuccess(error.response.success)
-        })
+      getLogData();
     }
-  }, [getLog])
+  }, [logID]);
 
   return (
     <div>
-      <ul>
+      <div className='title'>
         <h1>quicklogs</h1>
-        <input value={logID} onChange={e => {
-          setLogID(e.target.value)
-        }} /><br/>
-        <div>
-          {success === true && logID !== '' ?
-            <div>
-              <a href={`https://logs.tf/${logID}`} target='_blank'>{tfLogs.title}</a>
-              <button className='remove-focus' onClick={handleSaveLog}>+</button><br /><br />
-            </div>
-            :
-            <h2>Log not found!</h2>}
-          <div style={{ textAlign: 'center' }}>
-            {createdLogs.map((createdLog) => {
-              return (
-                <div>
-                  <a href={`https://logs.tf/${createdLog.log_id}`} target='_blank'>{createdLog.title}</a><br />
-                </div>
-              )
-            })}
+        <input type='number' pattern='[0-9]*' placeholder='Log ID' className='input-box' onChange={(e) => setLogID(e.target.value)} />
+        {loading ? (
+          <></>
+        ) : logID !== '' && success === true ? (
+          <div style={{ top: '-100px' }}>
+            <a href={`https://logs.tf/${logID}`} target='_blank'>{tfLogs.title}</a>
+            <img src='/plus-button.png' alt='Add log' className='add-button' onClick={handleSaveLog} />
           </div>
-        </div>
-      </ul>
+        ) : logID === '' ? (
+          <></>
+        ) : (
+          <h2 style={{ top: '-100px' }}>Log not found!</h2>
+        )}
+      </div>
+      <div>
+        {createdLogs.map((createdLog) => {
+          return (
+            <>
+              <a href={`https://logs.tf/${createdLog.log_id}`} target='_blank'>{createdLog.title}</a><br />
+            </>
+          )
+        })}
+      </div>
     </div>
-  )
+  );
 }
 
 export default App
